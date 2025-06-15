c6c4d95fa6c2cd9686935a9c4a9a1f82e9ba4822
@patriciobe <patriciovaldez944@gmail.com>
2025-06-15 14:02:30 -0500

    mmm

--- js/preload-components.js
+++ js/preload-components.js
@@ -1,9 +1,9 @@
     async function cargarComponentes() {
       try {
-        const nav = await fetch('http://127.0.0.1:5500/components/nav.html');
+        const nav = await fetch('https://webdisgn.github.io/CalculadoraDeVentanasAluminio/components/nav.html');
         document.getElementById('nav-container').innerHTML = await nav.text();
 
-        const footer = await fetch('http://127.0.0.1:5500/components/footer.html');
+        const footer = await fetch('https://webdisgn.github.io/CalculadoraDeVentanasAluminio/components/footer.html');
         document.getElementById('footer-container').innerHTML = await footer.text();
       } catch (error) {
         console.error('Error cargando componentes:', error);
