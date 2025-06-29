77a094d3e8b2198cc6a6574a1bfc68d139ab8f67
@patriciobe <patriciovaldez944@gmail.com>
2025-06-29 10:26:13 -0500

    v3.2

--- js/precios.js
+++ js/precios.js
@@ -1,6 +1,8 @@
     document.addEventListener("DOMContentLoaded", function () {
       let precio_riel = document.getElementById("precio_riel");
       let precio_riel_r = document.getElementById("precio_riel_r");
+      let precio_jamba = document.getElementById("precio_jamba");
+      let precio_jamba_r = document.getElementById("precio_jamba_r");
       // Capturar cambio en el input del color
       const colorInput = document.getElementById("aluminioColor");
 
@@ -9,6 +11,14 @@
           alert("Has seleccionado el color blanco");
           precio_riel.textContent = "25";
           precio_riel_r.textContent = "38";
+        } else if(colorInput.value === "negro"){
+          alert("Color Negro seleccioando");
+        } else if(colorInput.value === "bronce"){
+          alert("Color bronce seleccionado");
+        } else if(colorInput === "natural"){
+          alert("Color Natural Seleccionado");
+        } else if (colorInput === "maderado"){
+          alert("Color Maderado seleccionado");
         }
       });
     });
